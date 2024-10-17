ac705b80c00c3e28f49684b51d8d3137254364d8
tiiffanyho <Tiffanyho0903@gmail.com>
2024-10-17 13:10:46 -0400

    add label avatar and avatar manager

--- MenuScreen.java
+++ MenuScreen.java
@@ -4,7 +4,8 @@ public class MenuScreen extends World {
   public MenuScreen() {
     super(600, 400, 1);
 
-    addObject(new Button(this::goInstructions), 300, 340);
+    //addObject(new Button(this::goInstructions), 300, 340);
+    addObject(new Button(() -> Greenfoot.setWorld(new InstructionScreen(this))), 300, 340);
   }
 
   public void goInstructions() {
