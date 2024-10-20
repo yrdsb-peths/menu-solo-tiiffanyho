459723a880c94fcbcad9cb01e66ae9a64e54eb70
tiiffanyho <Tiffanyho0903@gmail.com>
2024-10-20 19:28:16 -0400

    indentation

--- MenuScreen.java
+++ MenuScreen.java
@@ -1,15 +1,15 @@
 import greenfoot.*;
 
-public class MenuScreen extends World {
-  public MenuScreen() {
-    super(600, 400, 1);
-
-    //addObject(new Button(this::goInstructions), 300, 340);
-    addObject(new Button(() -> Greenfoot.setWorld(new InstructionScreen(this))), 300, 340);
-  }
-
-  public void goInstructions() {
-    Greenfoot.setWorld(new InstructionScreen(this));
-  }
-
+public class MenuScreen extends World 
+{
+    public MenuScreen() 
+    {
+        super(600, 400, 1);
+        addObject(new Button(() -> Greenfoot.setWorld(new InstructionScreen(this))), 300, 340);
+    }
+    
+    public void goInstructions() 
+    {
+        Greenfoot.setWorld(new InstructionScreen(this));
+    }
 }
