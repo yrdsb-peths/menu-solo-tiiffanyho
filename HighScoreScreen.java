c8c2456b95c580433dc658d4ebca3bdbcfb25a11
tiiffanyho <Tiffanyho0903@gmail.com>
2024-10-20 22:29:04 -0400

    highscorescreen contructor and display

--- HighScoreScreen.java
+++ HighScoreScreen.java
@@ -1,15 +1,31 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; 
+import java.util.HashMap;
+import java.util.Map;
 
 public class HighScoreScreen extends World
 {
-
-    /**
-     * Constructor for objects of class HighScoreScreen.
-     * 
-     */
+    private Map<String, Integer> highScores;
+    
     public HighScoreScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        highScores = new HashMap<>();
+        highScores.put("Gloriana", 1200);
+        highScores.put("Tiffany", 950);
+        highScores.put("Natalie", 800);
+        displayHighScores();
+    }
+    
+    private void displayHighScores()
+    {
+        int y = 150; 
+        for (Map.Entry<String, Integer> entry : highScores.entrySet()) {
+            String name = entry.getKey();
+            Integer score = entry.getValue();
+            Label scoreLabel = new Label(name + ": " + score, 30);
+            addObject(scoreLabel, getWidth() / 2, y);
+            y += 50;
+        }
     }
 }
