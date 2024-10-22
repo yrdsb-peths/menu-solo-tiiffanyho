36e8c4f5e7a3e495db8f4a4b3adaa8b7b7a12d5f
tiiffanyho <Tiffanyho0903@gmail.com>
2024-10-21 22:43:27 -0400

    cleaning

--- HighScoreScreen.java
+++ HighScoreScreen.java
@@ -3,8 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+
 
 public class HighScoreScreen extends World
 {
--- InstructionScreen.java
+++ InstructionScreen.java
@@ -1,11 +1,5 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  
 
-/**
- * Write a description of class InstructionScreen here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class InstructionScreen extends World
 {
     private World home;
